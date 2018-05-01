e55f9e74d2aadded36a031a681c294c0fa8414c2
LBWright <lwright.js@gmail.com>
2018-05-01 13:15:30 -0600

    created Users model. Register route now works appropriately. Users are appropriately saved to the database

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+//bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 //mongo config
 const db = require('./config/keys').mongoURI;
 
 //connect to db
 mongoose.connect(db)
-	.then(() => console.log('MongoDB Connected'))
-	.catch((err) => console.log(err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log(err));
 
 //create express routes
 app.get('/', (req,res) => {
-	res.send('hello world');
+    res.send('hello world');
 });
 
 //use routes
@@ -26,6 +30,6 @@ app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
-	console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 
 });
