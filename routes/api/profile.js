d3865ef2955ad36904c3edbba292ab8ae324680e
LBWright <lwright.js@gmail.com>
2018-05-03 18:11:37 -0600

    created profile schema and added profile base route with athentication middleware

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//load Profile model
+const Profile = require('../../models/Profile');
+//load User model
+const User = require('../../models/User');
+
+
 
 //@route    GET api/profile/test
 //@desc     tests profile route
@@ -8,6 +17,22 @@ router.get('/test', (req,res) =>{
     res.json({
         msg: 'Profile works'
     });
+});
+
+//@route    GET api/profile
+//@desc     get current user's profile
+//@access   Private
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const errors = {};
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There is no profile';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
