ab10fb68df9041637dab9682db17710a2af5f051
LBWright <lwright.js@gmail.com>
2018-05-05 00:10:00 -0600

    added a Post model and gave the ability to add comments to a post. Created post and comment validation.

---
+++ validation/post.js
@@ -0,0 +1,20 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data){
+	let errors = {};
+	
+	data.text = !isEmpty(data.text) ? data.text : '';
+
+	//text validation
+	if (!Validator.isLength(data.text, {min: 10, max: 400})){
+		errors.text = 'Post content must be between 10 and 400 characters';
+	}
+	if (Validator.isEmpty(data.text)){
+		errors.text = 'Text field is required';
+	}
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
