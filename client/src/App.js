0feeee2fc6cb9d7fdc0d2a0045a7bbedfd3a016f
LBWright <lwright.js@gmail.com>
2018-05-15 16:59:21 -0500

    added authentication rerouting to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -21,6 +21,13 @@ if (localStorage.jwtToken){
   const decoded = jwt_decode(localStorage.jwtToken);
   //set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime){
+    store.dispatch(logoutUser());
+    //TODO: clear current profile
+    window.location.href = '/login';
+  }
 }
 
 
